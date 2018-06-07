1c58821b23964a4da3be73766977628fba133312
Trevor Kirpaul <trevorkirpaul@gmail.com>
2018-06-07 07:47:14 -0400

    added linting and blog post delete method and route

--- controllers/BlogPost.js
+++ controllers/BlogPost.js
@@ -1,22 +1,29 @@
-const BlogPost = require('../models/blogPost')
+const BlogPost = require("../models/blogPost");
 
 exports.create = (req, res, next) => {
-  const blogPost = req.body
+  const blogPost = req.body;
   BlogPost.create(blogPost)
     .then(blogpost => res.status(201).send({ blogpost }))
-    .catch(err => res.status(400).send({ err }))
-}
+    .catch(err => res.status(400).send({ err }));
+};
 
 exports.fetchAllPosts = (req, res, next) => {
   BlogPost.find({})
-    .populate('author')
+    .populate("author")
     .then(posts => res.status(200).send({ posts }))
-    .catch(err => res.status(400).send({ err }))
-}
+    .catch(err => res.status(400).send({ err }));
+};
 
 exports.fetchPostByID = (req, res, next) => {
-  const blogPostID = req.body.blogPostID
+  const blogPostID = req.body.blogPostID;
   BlogPost.findById(blogPostID)
     .then(blogPost => res.send({ blogPost }))
-    .catch(err => res.status(400).send({ err }))
-}
+    .catch(err => res.status(400).send({ err }));
+};
+
+exports.deleteBlogPost = (req, res, next) => {
+  const blogPostID = req.body.blogPostID;
+  BlogPost.findByIdAndRemove(blogPostID)
+    .then(post => res.status(201).send({ post }))
+    .catch(err => res.status(400).send({ err }));
+};
--- routes/routes.js
+++ routes/routes.js
@@ -1,35 +1,37 @@
-const UserController = require('../controllers/User')
-const BlogPostController = require('../controllers/BlogPost')
-const passportService = require('../services/passport')
-const passport = require('passport')
-const reqSignIn = passport.authenticate('local', { session: false })
-const requireAuth = passport.authenticate('jwt', { session: false })
+const UserController = require("../controllers/User");
+const BlogPostController = require("../controllers/BlogPost");
+const passportService = require("../services/passport");
+const passport = require("passport");
+const reqSignIn = passport.authenticate("local", { session: false });
+const requireAuth = passport.authenticate("jwt", { session: false });
 
 module.exports = app => {
-  app.get('/', (req, res, next) => {
-    res.send({ message: 'This is the parent level API' })
-  })
+  app.get("/", (req, res, next) => {
+    res.send({ message: "This is the parent level API" });
+  });
 
   // ? USER
 
   // CREATE
-  app.post('/user', UserController.create)
+  app.post("/user", UserController.create);
 
   // SIGN IN
-  app.post('/signin', reqSignIn, UserController.signIn)
+  app.post("/signin", reqSignIn, UserController.signIn);
 
   // AUTH
-  app.post('/auth', UserController.authUser)
+  app.post("/auth", UserController.authUser);
 
   // ? BLogPost
 
   // CREATE
-  app.post('/blogpost', BlogPostController.create)
+  app.post("/blogpost", BlogPostController.create);
 
   // FETCH ALL
-  app.get('/blogposts', BlogPostController.fetchAllPosts)
+  app.get("/blogposts", BlogPostController.fetchAllPosts);
 
   // FETCH BY ID
-  app.post('/blogpost/fetch', BlogPostController.fetchPostByID)
+  app.post("/blogpost/fetch", BlogPostController.fetchPostByID);
 
-}
+  // DELETE BY ID
+  app.delete("/blogpost", BlogPostController.deleteBlogPost);
+};
